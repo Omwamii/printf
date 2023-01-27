d8872b9f864bc366c67e71ceffc1ddec62d7ed18
Omwamii <ianalpha@protonmail.com>
2023-01-27 19:06:22 +0300

    few local changes to printf.c

--- 1-printf.c
+++ 1-printf.c
@@ -9,13 +9,12 @@
   *
   *Return: characters printed
   */
-
 int _printf(const char *format, ...)
 {
 	va_list input;
 	int i = 0, count = 0;
-	char ch;
-	char *str;
+	char newline = '\n', ch, *str;
+	signed int dec, deci;
 
 	va_start(input, format);
 
@@ -28,27 +27,55 @@ int _printf(const char *format, ...)
 			{
 				case 'c':
 					ch = va_arg(input, int);
-
 					write(1, &ch, 1);
 					count = 1;
 					break;
 				case 's':
-					str = va_arg(input, char*);
+				str = va_arg(input, char*);
 
-					while (str[count] != '\0')
+				while (str[count] != '\0')
 					{
 						write(1, &str[count], 1);
 						count++;
 					}
 					break;
+				case '\n':
+					write(1, &newline, 1);
+					break;
+				case 'i':
+					dec = va_arg(input, signed int);
+					write(1, &dec, sizeof(dec));
+					break;
+				case 'd':
+					deci = va_arg(input, signed int);
+					write(1, &deci, sizeof(deci));
+					break;
 				default:
 					break;
 			}
+			i++;
 		}
 
-		write(1, &format[i], 1);
-		i++;
+		else
+		{
+			write(1, &format[i], 1);
+			i++;
+		}
 	}
 	va_end(input);
 	return (count);
+}
+
+/**
+  *main - entry point check
+  */
+
+int main(void)
+{
+	int c = -2;
+
+	_printf("%d\n",c);
+	_printf("The teacher said %i should see him today\n", c);
+
+	return (0);
 }
