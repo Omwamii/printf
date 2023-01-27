6875e555f1bd618bbc97d3b3f2da46ef218b331e
Omwamii <ianalpha@protonmail.com>
2023-01-27 13:27:45 +0300

    task 0 handling

--- main.h
+++ main.h
@@ -6,6 +6,17 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stddef.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
 
 int _printf(const char *format, ...);
 int (*check_specifier(char fmt))(va_list);
