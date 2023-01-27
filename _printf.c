feab996c6b77b9c9f2a9b3c6cb173429f343edd2
Shikanda <116168041+Shikanda@users.noreply.github.com>
2023-01-27 03:54:39 +0300

    trial

--- _printf.c
+++ _printf.c
@@ -1,88 +1,92 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "main.h"
-
+#include <stdarg.h>
+#include <stdio.h>
 /**
-* struct print_format - Struct containing the conversion specifier and corresponding function
-* @c: conversion specifier
-* @func: function to handle the conversion
+*_printf - prints a string in format
+*@format: first argument
+*@...: variable list of arguments
+*Return: args
+*
 */
-typedef struct print_format
-	{
-		char c;
-		void (*func)(va_list);
-	} print_format_t;
-
 int _printf(const char *format, ...)
 {
 	va_list args;
 
-    	int i;
+	int i;
+	int j;
 	int count;
 
 	i = 0;
+	j = 0;
 	count = 0;
-    	print_format_t formats[] = {
-        	{'c', print_char},
-        	{'s', print_string},
-        	{'%', print_percent},
-        	{0, NULL}
-    	};
+	print_format_t formats[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_percent},
+		{0, NULL}
+	};
 
-    	va_start(args, format);
-    	while (format[i])
-    	{
-        	if (format[i] == '%')
-        	{
-            	i++;
-            	int j = 0;
-            	while (formats[j].c)
-            	{
-                	if (formats[j].c == format[i])
-                	{
-                    	formats[j].func(args);
-                    	count++;
-                    	break;
-                	}
-                	j++;
-            	}
-        	}
-        	else
-        	{
-		_putchar(format[i]);
-		count++;
+	va_start(args, format);
+	while (format[i]);
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			while (formats[j].c)
+			{
+				if  (formats[j].c == format[i])
+				{
+					formats[j].func(args);
+					count++;
+					break;
+				}
+				j++;
+			}
+		}
+		else
+		[
+			_putchar(format[i]);
+			count++;
 		}
 		i++;
 	}
 	va_end(args);
-	return count;
+	return (count);
 }
-
 /**
-* print_char - prints a single character
-* @args: list of arguments
+*print_char - prints a single character
+*@args: list of arguments
+*
+*Return: always 0
 */
 void print_char(va_list args)
 {
 	char c = va_arg(args, int);
-	_putchar(c);
+	_putchar(s);
 }
-
 /**
-* print_string - prints a string of characters
-* @args: list of arguments
+*print_string - prints a string of characters
+*@args: list of arguments
+*Return: a string
+*
 */
 void print_string(va_list args)
 {
-	char *s = va_arg(args, char*);
+	char *s = va_arg(args, char *);
 	_putchar(s);
 }
-
 /**
-* print_percent - prints a percent character
-* @args: list of arguments (unused)
+*print_percent - prints a percent character
+*@args: list of arguments
+*Return: nun
+*
 */
 void print_percent(va_list args)
 {
 	_putchar('%');
 }
+
+
+
+
+
