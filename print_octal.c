5750eac16bdbab817ce6260642fb053e0aefab79
Omwamii <ianalpha@protonmail.com>
2023-01-29 00:51:29 +0300

    added octal handling'

--- print_octal.c
+++ print_octal.c
@@ -17,7 +17,7 @@ int to_octal(int num)
 		octal[i] = (num % 8);
 		deci /= 8;
 		i++;
-		octal++;
+		octal++; /* possible issue: this may end up passing last index */
 	}
 	j = i - 1;
 	i = 0;
@@ -44,6 +44,20 @@ int to_octal(int num)
 int print_octal(va_list input)
 {
 	int deci = va_arg(input, int);
+	int *oct, written = 0, i = 0;
 
+	if (deci > 7)
+	{
+		oct = to_octal(deci);
+
+		while (oct[i] != '\0')
+		{
+			written += write(1, &oct[i], 1);
+			i++;
+		}
+		return (written);
+	}
+
+	return (write (1, &deci, 1));
 
 }
