fe0c364d43453a3fafc5af22772c216289e96d2c
Shikanda <116168041+Shikanda@users.noreply.github.com>
2023-01-28 17:07:54 +0300

    trial 2

---
+++ printf.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "main.h"
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+*_printf - produce output according to format
+*@format: string to be output
+*...: other variables
+*Return: number of characters that are printed out
+ */
+int _printf(const char *format, ...)
+{
+	int count;
+	int (*f)(va_list);
+	va_list args;
+
+	count = 0;
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%' && *(format + 1) != '%')
+		{
+			format++;
+			f = check_specifier(format);
+			if (*(format) == '\0')
+				return (-1);
+			else if (f == NULL)
+			{
+				_putchar(*(format - 1));
+				_putchar(*(format));
+				count += 2;
+			}
+			else
+			{
+				count += f(args);
+			}
+		}
+		else if (*format == '%' && *(format + 1) == '%')
+		{
+			format++;
+			_putchar('%');
+			count++;
+		}
+		else
+		{
+			_putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
