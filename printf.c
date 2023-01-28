0df029745de7d80528c12ffe5d603aa15e82b856
Omwamii <ianalpha@protonmail.com>
2023-01-28 18:08:13 +0300

    handling %b

--- printf.c
+++ printf.c
@@ -8,10 +8,11 @@
 
 int *tobinary(int dec)
 {
-	int bin[50], i = 0, num = dec;
+	int *bin, i = 0, num = dec;
 
 	while (num > 0)
 	{
+		bin[i] = malloc(sizeof(int));
 		bin[i] = (num % 2);
 		num /= 2;
 		i++;
